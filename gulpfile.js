b29cf0c2dfaacbb2f9ed2c701f50f78ce5756af5
Brenda Uga <brendauga@gmail.com>
2018-03-23 19:27:01 +0100

    Refactor to use html-include with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,21 @@ var cleancss = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var fileinclude = require('gulp-file-include');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('html', function() {
+  gulp.src(['index.template.html', '*/index.template.html'], {base: './'})
+      .pipe(rename(function(opt) {
+        opt.basename = opt.basename.replace(/\.template/, '');
+        return opt;
+      }))
+      .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+      }))
+      .pipe(gulp.dest("./"));
+});
 
 gulp.task('sass', function() {
   gulp.src('resources/scss/*.scss')
@@ -17,11 +32,6 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('resources/scss/*.scss', ['sass']);
-  gulp.watch('resources/js/app.js', ['scripts']);
-});
-
 gulp.task('scripts', function() {
   var jsMinifyLocation = ['resources/js/*.js', '!resources/js/*.min.js'];
   gulp.src(jsMinifyLocation)
@@ -30,4 +40,10 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('default', ['sass', 'scripts', 'watch']);
+gulp.task('watch', function() {
+  gulp.watch(['index.template.html', '*/index.template.html'], ['html']);
+  gulp.watch('resources/scss/*.scss', ['sass']);
+  gulp.watch('resources/js/app.js', ['scripts']);
+});
+
+gulp.task('default', ['sass', 'scripts', 'html', 'watch']);
